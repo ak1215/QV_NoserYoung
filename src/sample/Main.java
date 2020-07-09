8aad1b93d67060246bbfe9ef77b8dd7256139921
ak1215 <60421957+ak1215@users.noreply.github.com>
2020-07-09 15:31:33 +0200

    09.07.2020 15:30 DB Connection

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -6,10 +6,12 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
+import java.sql.*;
+
 public class Main extends Application {
 
     @Override
-    public void start(Stage primaryStage) throws Exception{
+    public void start(Stage primaryStage) throws Exception {
         Parent root = FXMLLoader.load(getClass().getResource("Main.fxml"));
         primaryStage.setTitle("Hello World");
         primaryStage.setScene(new Scene(root, 300, 275));
@@ -18,5 +20,26 @@ public class Main extends Application {
 
     public static void main(String[] args) {
         launch(args);
+        String url = "jdbc:mysql://192.168.205.128:3306";
+        String uname = "root";
+        String pass = "secret";
+        try {
+            Class.forName("org.mariadb.jdbc.Driver");
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+        try {
+            Connection myCon = DriverManager.getConnection(url, uname, pass);
+            Statement myState = myCon.createStatement();
+            String sql = "select  * from hr.employee";
+            ResultSet rs = myState.executeQuery(sql);
+
+            while (rs.next()) {
+                System.out.println(rs.getString("last_name"));
+            }
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 }
